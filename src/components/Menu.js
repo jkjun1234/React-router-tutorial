55565e7c6c33b8ef83c6fc7ac77afdfa055f8172
jkjun1234 <jkjun1234@gmail.com>
2019-08-11 17:56:21 +0900

    Router 링크 설정 방법 확인

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Menu = () => {
+    const activeStyle = {
+        color: 'green',
+        fontSize: '2rem'
+    };
+
+    //NavLink는 해당 URL이 활성화 시 특정 스타일 또는 클래스를 지정가능
     return (
         <div>
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/about/foo">About Foo</Link></li>
+                <li><NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink></li>
+                <li><NavLink exact to="/about" activeStyle={activeStyle}>About</NavLink></li>
+                <li><NavLink to="/about/foo" activeStyle={activeStyle}>About Foo</NavLink></li>
+                <li><NavLink to="/posts" activeStyle={activeStyle}>Posts</NavLink></li>
             </ul>
             <hr/>
         </div>
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';         // Switch 컴포넌트는 라우트들을 매칭되는 첫번째 라우트만 보여준다.
-import {Home, About} from 'pages';
+import {Home, About, Posts} from 'pages';
 import Menu from "components/Menu";
 
 class App extends Component {
@@ -10,9 +10,10 @@ class App extends Component {
                 <Menu/>
                 <Route exact path="/" component={Home}/>    {/**exact 는 주어진경로와 정확히 일치하여야 컴포넌트를 보여준다. */}
                 <Switch>
-                    <Route path="/about/:name" component={About}/>      {/** 먼저 비교할 라우트를 작성하여야 비교하여 나타내준다. */}
+                    <Route path="/about/:name" component={About}/>    {/** 먼저 비교할 라우트를 작성하여야 비교하여 나타내준다. */}
                     <Route path="/about" component={About}/>
                 </Switch>
+                <Route path="/posts" component={Posts}/>
             </div>
         );
     }
